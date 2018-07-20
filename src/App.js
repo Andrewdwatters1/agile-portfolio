91d427074ca19d86fcfd8069e283114f4c7f55bf
Andrew Watters <Andrewdwatters1@gmail.com>
2018-07-20 14:12:58 -0600

    Basic layout set, header and main content boxes rendered

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './CSS/main.css';
+import Header from './Components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="outer-flex">
+
+        <div class="content-right-main">
+          <Header/>
+          <div>Portfolio Container
+            {/* <Input/>
+            <BuySell/>
+            <Display/> */}
+           </div>
+           {/* <Footer/> */}
+        </div>
+
+        <div class="content-left-main">Left Sidebar
+          {/* <MarketIndexes/>
+          <Search/>
+          <Watchlist/> */}
+        </div>
+
       </div>
     );
   }
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+const Header = function myHeader(props) {
+  return (
+  <div class="header-main">
+    <h1>Welcome to Agile Portfolio!</h1>
+    <h3>To get started: Enter your holdings below </h3>
+  </div>
+)};
+
+export default Header;
+
+// FUNCTION COMPLETE, NEEDS STYLED
+
