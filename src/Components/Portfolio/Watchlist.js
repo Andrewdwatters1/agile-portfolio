cadb5bfd5b9fae2f431e7b596c9721e0caf72b19
Andrew Watters <Andrewdwatters1@gmail.com>
2018-07-21 15:53:30 -0600

    Watchlist component renders desired outputs, need formatted still

--- src/Components/Portfolio/Watchlist.js
+++ src/Components/Portfolio/Watchlist.js
@@ -11,16 +11,13 @@ class Watchlist extends Component {
   }
 
   render() {
+    var watchlist = [];
     for (let i = 0; i < this.props.stocksList.length; i++) {
-      console.log("Symbol:", this.props.stocksList[i]["Meta Data"]["2. Symbol"].toUpperCase(), "Last Updated:", this.props.stocksList[i]["Meta Data"]["3. Last Refreshed"], "Price:", this.props.stocksList[i]["Time Series (1min)"]["2018-07-20 16:00:00"]["4. close"]);
+      watchlist.push(`Symbol: ${this.props.stocksList[i]["Meta Data"]["2. Symbol"].toUpperCase()}, Updated: ${this.props.stocksList[i]["Meta Data"]["3. Last Refreshed"]}, Price: ${this.props.stocksList[i]["Time Series (1min)"]["2018-07-20 16:00:00"]["4. close"]}`);
     }
     return (
       <div>
-          {/* {console.log("Watchlist props", this.props)} */}
-          jkasdkl;sadj;<br />
-          asdfasdfasdfa<br />
-          asdfasdfasdff<br />
-          ;lkajsdj;adsj<br />
+          {JSON.stringify(watchlist)}
       </div>
     )
   }
